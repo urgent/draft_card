a4f16c58bf75e43bb4e242aa080e5c71254c58cf
urgent <urgent.programmer@gmail.com>
2021-02-18 13:50:34 -0500

    Testing netlify functions
    Not running locally with dev

--- netlify/functions/pick.js
+++ netlify/functions/pick.js
@@ -1,13 +1,28 @@
-const query = require('../includes/graphql')
+const graphql = require('../includes/graphql')
+
+const DraftQuery = `query DraftQuery {
+  drafts_by_pk(id:1) {
+    draft_order
+    interval
+    start
+  }
+}`;
 
 exports.handler = async function (event, context) {
-    const payload = await query.fetchQuery(`query MyQuery {
-        teams {
-          name
-        }
-      }`)
-    return {
-        statusCode: 200,
-        body: JSON.stringify(payload)
-    };
+  // is username post set, validate post
+  const payload = await graphql.fetchQuery(DraftQuery)
+
+  // validate draft
+
+  // is draft over
+
+  // authenticate, is username in draft order
+
+  // is user in post the draft's next pick?
+
+  // 
+  return {
+    statusCode: 200,
+    body: JSON.stringify(payload)
+  };
 }
--- src/function_test/pick.test.js
+++ src/function_test/pick.test.js
@@ -1,5 +1,7 @@
 import { handler } from '../../netlify/functions/pick';
 
-test('Pick works for draft spot', () => {
-    expect(true).toEqual(true);
+test('Pick blocks empty POST', async () => {
+    const response = await handler();
+    const json = JSON.parse(response.body)
+    expect(json.data).toEqual({ result: 'error' });
 })
